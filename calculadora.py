2f2e33ef4ada8e03c787e52a4c202c8078459129
Luiz <jc381311@gmail.com>
2025-09-14 23:56:32 -0300

    My first calculator

---
+++ calculadora.py
@@ -0,0 +1,39 @@
+def calculator():
+    print("Calculadora em Python")
+    print("(Operações disponíveis: \n '+' Adição \n '-' Subtração \n '*' Multiplicação \n '/' Divisão) \n 'E' para cancelar.")
+
+# '+' Adição
+# '-' Subtração
+# '*' Multiplicação
+# '/' Divisão
+# 'E' Sair da operação
+
+while True:
+    number_1 = float(input('Digite o primeiro número: '))
+    operator = input('Digite o operador (+, -, *, /): E para cancelar. ')
+
+    if operator.upper() == 'E':
+        print("Operação encerrada!")
+        break
+
+    number_2 = float(input('Digite o segundo número: '))
+
+    if operator == '+':
+        result = number_1 + number_2
+    elif operator == '-':
+        result = number_1 - number_2
+    elif operator == '*':
+        result = number_1 * number_2
+    elif operator == '/':
+        if number_2 == 0:
+            print('[ERRO] Divisão por zero não é permitida!')
+            continue
+        else:
+            result = number_1 / number_2
+    else:
+        print('[ERRO] Operador inválido!')
+        continue
+
+    print(f'Resultado: {result:.2f}')
+
+calculator()
